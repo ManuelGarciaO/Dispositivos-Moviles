bda8fc83b2a288243cfc67cfb22c8e9ffb3947e5
manuel <manuelobezo@gmail.com>
2020-06-09 17:40:03 -0500

    fixes

---
+++ app/src/main/java/com/example/sportsapps/partidos.java
@@ -0,0 +1,13 @@
+package com.example.sportsapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class partidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partidos);
+    }
+}
